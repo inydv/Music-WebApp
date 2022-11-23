2a3893542074db42cf575b87def4e931a7454282
inydv <i_nydv@hotmail.com>
2022-11-23 20:15:46 +0530

    adding rapid api

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', process.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
+            return headers;
+        }
+    }),
+
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({
+            query: 'charts/world'
+        })
+    })
+})
+
+export const { useGetTopChartQuery } = shazamCoreApi;
