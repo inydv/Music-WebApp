4500f6cd5578dfd28139db8ddd31d0eb00131de5
inydv <i_nydv@hotmail.com>
2022-11-24 18:32:57 +0530

    top play and top artist

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -47,7 +47,7 @@ const Sidebar = () => {
       </div>
 
       <div
-        className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${
+        className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#1C1C1E] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${
           mobileMenuOpen ? "left-0" : "-left-full"
         }`}
       >
