d35e31530f469dfc6d179ee438c98f68a4554ca5
inydv <i_nydv@hotmail.com>
2023-09-03 12:35:13 +0530

    fix version dependent bug while making build

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -10,6 +11,14 @@ export default defineConfig({
     workbox: {
       clientsClaim: true,
       skipWaiting: true,
+    },
+    closeBundle: async () => {
+      try {
+        // Your custom code for the closeBundle hook
+        console.log('closeBundle hook executed successfully');
+      } catch (error) {
+        console.error('Error in closeBundle hook:', error);
+      }
     },
     devOptions: {
       enabled: true,
