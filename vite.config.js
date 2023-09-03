eb9a0bbf803385a09c0dec696e2cabfd6dae59bd
inydv <i_nydv@hotmail.com>
2023-09-03 09:44:33 +0530

    add vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,14 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    workbox: {
+      clientsClaim: true,
+      skipWaiting: true,
+    },
+    devOptions: {
+      enabled: true,
+    },
+  })],
 });
